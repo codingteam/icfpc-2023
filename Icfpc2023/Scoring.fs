583d5d86a2d15a196b139f2a710612568b7eb6a2
Friedrich von Never <friedrich@fornever.me>
2023-07-07 19:17:58 +0200

    Proper solver (I hope?)

--- Icfpc2023/Scoring.fs
+++ Icfpc2023/Scoring.fs
@@ -7,13 +7,24 @@ type private Musician = {
 }
 
 let private CalculateAttendeeMusicianScore (attendee: Attendee) (musician: Musician): double =
-    // TODO: Determine if musician is blocked by anyone else
     let d_squared = (attendee.X - musician.Location.X) ** 2.0 + (attendee.Y - musician.Location.Y) ** 2.0
     ceil(1_000_000.0 * attendee.Tastes[musician.Instrument] / d_squared)
 
+let private AnyOtherMusicianBlocksSound (musicians: Musician[]) (attendee: Attendee) (mIndex: int): bool =
+    let musician = PointD(musicians.[mIndex].Location.X, musicians.[mIndex].Location.Y)
+    let attendee = PointD(attendee.X, attendee.Y)
+    let blockZone = { Center1 = musician; Center2 = attendee; Radius = 5.0 }
+
+    Seq.indexed musicians
+    |> Seq.filter(fun (i, _) -> i <> mIndex)
+    |> Seq.exists(fun (_, m) -> blockZone.Contains(m.Location))
+
 let private CalculateAttendeeScore (musicians: Musician[]) (attendee: Attendee): double =
-    musicians
-    |> Array.sumBy(CalculateAttendeeMusicianScore attendee)
+    Seq.indexed musicians
+    |> Seq.sumBy(fun (i, musician) ->
+        if AnyOtherMusicianBlocksSound musicians attendee i then 0.0
+        else CalculateAttendeeMusicianScore attendee musician
+    )
 
 let CalculateScore(problem: Problem) (solution: Solution): double =
     let musicians =
