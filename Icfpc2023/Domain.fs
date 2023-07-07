dfce93fbe02d74e0684ca120ac24253e5a706ff5
Friedrich von Never <friedrich@fornever.me>
2023-07-07 16:16:53 +0200

    Domain: add main types

---
+++ Icfpc2023/Domain.fs
@@ -0,0 +1,29 @@
+﻿namespace Icfpc2023
+
+open Newtonsoft.Json
+
+[<Struct>]
+type PointD =
+    | PointD of double * double
+    member this.X = match this with PointD(x, _) -> x
+    member this.Y = match this with PointD(_, y) -> y
+
+type Attendee = {
+    X: double
+    Y: double
+    Tastes: double[]
+}
+
+type Problem = {
+    RoomWidth: double
+    RoomHeight: double
+    StageWidth: double
+    StageHeight: double
+    StageBottomLeft: PointD
+    Musicians: int[]
+    Attendees: Attendee[]
+}
+
+type Solution = {
+    Placements: PointD[]
+}
