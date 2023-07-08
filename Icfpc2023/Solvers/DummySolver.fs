a84373d479548af7d836e27aa1df082e5a59ca84
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2023-07-08 12:45:50 +0400

    Use both versions of DummySolver

--- Icfpc2023/Solvers/DummySolver.fs
+++ Icfpc2023/Solvers/DummySolver.fs
@@ -1,6 +1,17 @@
 module Icfpc2023.DummySolver
 
-let Solve(problem: Problem): Solution =
+let SolveV1(problem: Problem): Solution =
+    let vacantRadius = 10.0
+    let grid = seq {
+        for x in vacantRadius .. vacantRadius .. problem.StageWidth-vacantRadius do
+            for y in vacantRadius .. vacantRadius .. problem.StageHeight-vacantRadius ->
+                problem.StageBottomLeft + PointD(x, y)
+    }
+    {
+        Placements = Seq.take problem.Musicians.Length grid |> Seq.toArray
+    }
+
+let SolveV2(problem: Problem): Solution =
     let vacantRadius = 10.0
     let xStep = vacantRadius
     let yStep = vacantRadius * sqrt 3.0
