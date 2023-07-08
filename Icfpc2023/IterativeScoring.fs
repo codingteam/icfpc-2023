7143818e4f0e94b89f3f6587d44350497fcba6a4
Alexander Batischev <eual.jp@gmail.com>
2023-07-09 00:59:18 +0300

    Store musician positions

--- Icfpc2023/IterativeScoring.fs
+++ Icfpc2023/IterativeScoring.fs
@@ -1,7 +1,12 @@
 module Icfpc2023.IterativeScoring
 
+open System.Collections.Immutable
+
+type private MusicianPlacements = PointD[]
+
 type State = {
-    // TODO: add a vector of musician placements [0]
+    Problem: Problem
+    MusicianPlacements: ImmutableArray<PointD> // [0]
 
     // TODO: add a matrix of attendee-musician distances (squared) -- depends on 0. [1]
 
@@ -16,9 +21,18 @@ type State = {
     // TODO: add matrix of closeness*impact (for each attendee-musician pair) -- depends on 4 and 2. [5]
     }
 
-let InitState(problem: Problem, musician_placements: PointD[]): State = failwith "unimplemented"
+let InitState(problem: Problem, musician_placements: PointD[]): State =
+    {
+        Problem = problem
+        MusicianPlacements = musician_placements.ToImmutableArray()
+    }
 
 /// Put musician at a given place and return updated state. Leave the original state unmodified
-let PlaceMusician(state: State, musicianId: int, place: PointD): State = failwith "unimplemented"
+let PlaceMusician(state: State, musicianId: int, place: PointD): State =
+    let new_musician_placements = state.MusicianPlacements.SetItem(musicianId, place)
+    // TODO: signal to other fields that this musician moved so they can re-calculate stuff
+    { state with
+        MusicianPlacements = new_musician_placements
+    }
 
 let CalculateScore(state: State): Score = failwith "unimplemented"
