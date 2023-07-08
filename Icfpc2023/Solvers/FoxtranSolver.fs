b257506d36de905babe5e9c4eeaed191ac8437b7
Friedrich von Never <friedrich@fornever.me>
2023-07-08 14:17:55 +0200

    Calculate gridPerInstrument

--- Icfpc2023/Solvers/FoxtranSolver.fs
+++ Icfpc2023/Solvers/FoxtranSolver.fs
@@ -1,18 +1,39 @@
 module Icfpc2023.FoxtranSolver
 
+let private MusicianDeadZoneRadius = 10.0
+let private GridCellSize = MusicianDeadZoneRadius
+
+type private PartialSolution(position: PointD, instrument: int) =
+    interface IPartialSolution with
+        member _.GetPlacedMusicians(musicians: int[]) =
+            [| position, instrument |]
+
+let private MakeSolutionWithSingleMusician index point =
+    PartialSolution(point, index)
+
 let FoxtranSolveV1(problem: Problem): Solution =
-    let vacantRadius = 10.0
-    // grid over scene; compute effective potential for each musician
-    let grid = seq {
-      for x in vacantRadius .. vacantRadius .. problem.StageWidth-vacantRadius do
-        for y in vacantRadius .. vacantRadius .. problem.StageHeight-vacantRadius do
-          for musician in problem.Musicians ->
-            // Point -> [scores]
-    }
+    let instrumentCount = problem.Attendees[0].Tastes.Length
+    // TODO: Should we exclude the edge cells completely?
+    let gridWidth, gridHeight =
+        int <| ceil (float problem.StageWidth / GridCellSize),
+        int <| ceil (float problem.StageHeight / GridCellSize)
+    let createGrid() = Array2D.zeroCreate gridWidth gridHeight
+
+    let gridsPerInstrument = Array.init instrumentCount (fun _ -> createGrid())
+
+    for cellX in 0 .. gridWidth - 1 do
+      for cellY in 0 .. gridHeight - 1  do
+        for instrument in 0 .. instrumentCount-1 do
+          // TODO: Place the musician in the middle of the cell?
+          let cellPosition = PointD(x * GridCellSize, y * GridCellSize)
+          let solution = MakeSolutionWithSingleMusician instrument cellPosition
+          let score = Scoring.CalculateNoBlockingScore problem solution
+          gridsPerInstrument[instrument][cellX, cellY] <- score
+
     // for each musician type
     //   find maximum over all musicians
     //     take this point
     //     remove points around selected point (set large negative value to these grid points over all musiians)
     {
-        Placements = ... 
+        Placements = ...
     }
