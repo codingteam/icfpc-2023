d0ed2913615e511fca29009d8b01319fdc3a79f3
Friedrich von Never <friedrich@fornever.me>
2023-07-08 16:12:48 +0200

    FoxtranSolverV1 works now

--- Icfpc2023/Solvers/FoxtranSolver.fs
+++ Icfpc2023/Solvers/FoxtranSolver.fs
@@ -1,15 +1,73 @@
 module Icfpc2023.FoxtranSolver
 
+open System
+open System.Collections.Generic
+
 let private MusicianDeadZoneRadius = 10.0
 let private GridCellSize = MusicianDeadZoneRadius
 
 type private PartialSolution(position: PointD, instrument: int) =
     interface IPartialSolution with
-        member _.GetPlacedMusicians(musicians: int[]) =
+        member _.GetPlacedMusicians _ =
             [| position, instrument |]
 
 let private MakeSolutionWithSingleMusician index point =
     PartialSolution(point, index)
+
+let private GetMusicianIndicesPerInstrument problem: Dictionary<int, list<int>> =
+    let result = Dictionary<int, list<int>>()
+    for m in 0 .. problem.Musicians.Length - 1 do
+        let instrument = problem.Musicians[m]
+        match result.TryGetValue(m) with
+        | false, _ -> result[instrument] <- [m]
+        | true, existing ->
+            result[instrument] <- m :: existing
+
+    result
+
+let FindMaxValue grid =
+    let mutable max = Double.MinValue
+    let mutable maxPos = struct(-1, -1)
+    for x in 0 .. Array2D.length1 grid - 1 do
+        for y in 0 .. Array2D.length2 grid - 1 do
+            if grid[x, y] > max then
+                 max <- grid[x, y]
+                 maxPos <- x, y
+    struct(maxPos, max)
+
+let private ChooseBestInstrument availableInstruments (gridsPerInstrument: Score[,][]) =
+    availableInstruments
+    |> Seq.maxBy(fun i ->
+        let struct(_, v) = FindMaxValue gridsPerInstrument[i]
+        v
+    )
+
+let private ChooseBestPosition grid =
+    let struct(pos, _) = FindMaxValue grid
+    pos
+
+let private GridCoordToPhysicalCoord(struct(x, y)) =
+    PointD(double x * GridCellSize, double y * GridCellSize)
+
+#nowarn "25"
+let private PlaceMusicianAndDestroyPosition position
+                                            instrument
+                                            (placements: PointD array)
+                                            (musicianIndicesPerInstrument: Dictionary<int, list<int>>)
+                                            (gridsPerInstrument: Score[,][]) =
+    let musician :: rest = musicianIndicesPerInstrument[instrument]
+
+    // Set to target array:
+    placements[musician] <- GridCoordToPhysicalCoord position
+
+    // Clean up existing data on musician:
+    match rest with
+    | [] -> musicianIndicesPerInstrument.Remove instrument |> ignore
+    | _ -> musicianIndicesPerInstrument[instrument] <- rest
+
+    // Clean up existing data on grid cell:
+    let struct(x, y) = position
+    gridsPerInstrument[instrument][x, y] <- Double.MinValue
 
 let FoxtranSolveV1(problem: Problem): Solution =
     let instrumentCount = problem.Attendees[0].Tastes.Length
@@ -25,15 +83,16 @@ let FoxtranSolveV1(problem: Problem): Solution =
       for cellY in 0 .. gridHeight - 1  do
         for instrument in 0 .. instrumentCount-1 do
           // TODO: Place the musician in the middle of the cell?
-          let cellPosition = PointD(x * GridCellSize, y * GridCellSize)
+          let cellPosition = GridCoordToPhysicalCoord(cellX, cellY)
           let solution = MakeSolutionWithSingleMusician instrument cellPosition
           let score = Scoring.CalculateNoBlockingScore problem solution
           gridsPerInstrument[instrument][cellX, cellY] <- score
 
-    // for each musician type
-    //   find maximum over all musicians
-    //     take this point
-    //     remove points around selected point (set large negative value to these grid points over all musiians)
-    {
-        Placements = ...
-    }
+    let musicianIndicesPerInstrument = GetMusicianIndicesPerInstrument problem
+    let placements = Array.zeroCreate problem.Musicians.Length
+    while musicianIndicesPerInstrument.Count > 0 do
+        let instrument = ChooseBestInstrument musicianIndicesPerInstrument.Keys gridsPerInstrument
+        let position = ChooseBestPosition gridsPerInstrument[instrument]
+        PlaceMusicianAndDestroyPosition position instrument placements musicianIndicesPerInstrument gridsPerInstrument
+
+    { Placements = placements }
