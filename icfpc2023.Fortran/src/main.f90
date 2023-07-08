fd2cce16eb3d6125738488d104d78e22c015ff16
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-07-08 23:57:16 +0900

    Implement build taste matrix

--- icfpc2023.Fortran/src/main.f90
+++ icfpc2023.Fortran/src/main.f90
@@ -3,7 +3,8 @@ program main
   implicit none
   type(room_t) :: room
   character(len=1024) :: filename
-  filename = "test.txt"
+  filename = "test.ini"
   call room%load(filename)
   call room%print()
+  print *, room%score()
 end program main
