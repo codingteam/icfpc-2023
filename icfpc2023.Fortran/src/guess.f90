1e5ed9278caf836a3c24b3f7d80c27e1acb11989
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-07-10 00:19:41 +0900

    Add foxtran v1 solver

--- icfpc2023.Fortran/src/guess.f90
+++ icfpc2023.Fortran/src/guess.f90
@@ -2,6 +2,23 @@ module guess_mod
   use domain
   implicit none
 contains
+  subroutine restore_order(room, order)
+    type(room_t), intent(inout) :: room
+    integer(8) :: order(:)
+    type(musician_t) :: musician
+    integer :: i, j
+    do i = 1, room%N_musicians
+      if (room%musicians(i)%instrument == order(i)) cycle
+      do j = i + 1, room%N_musicians
+        if (room%musicians(j)%instrument == order(i)) then
+          musician = room%musicians(i)
+          room%musicians(i) = room%musicians(j)
+          room%musicians(j) = musician
+          exit
+        end if
+      end do
+    end do
+  end subroutine restore_order
   subroutine guess_v1(room)
     type(room_t), intent(inout) :: room
     integer :: t, i, j, instr
@@ -42,21 +59,46 @@ contains
     end do
     call restore_order(room, order)
   end subroutine guess_v1
-  subroutine restore_order(room, order)
+  subroutine guess_v2(room)
+    use grid_mod
     type(room_t), intent(inout) :: room
-    integer(8) :: order(:)
+    integer :: t, i, j, instr, layer
     type(musician_t) :: musician
-    integer :: i, j
+    integer(8), allocatable :: order(:)
+    integer(8) :: max_pos(1)
+    ! grids
+    real(8) :: minx, maxx, miny, maxy
+    type(grid_t), allocatable :: grid(:)
+    type(grid_t) :: additional
+    allocate(grid(room%N_instruments))
+    minx = room%stage_bottom_left%x + 10
+    maxx = room%stage_bottom_left%x + room%stage_width - 10
+    miny = room%stage_bottom_left%y + 10
+    maxy = room%stage_bottom_left%y + room%stage_height - 10
+    do instr = 1, room%N_instruments
+      call grid(instr)%generate_grid(minx, maxx, miny, maxy, "rectangular", 10._8)
+    end do
+    allocate(order(room%N_musicians))
     do i = 1, room%N_musicians
-      if (room%musicians(i)%instrument == order(i)) cycle
-      do j = i + 1, room%N_musicians
-        if (room%musicians(j)%instrument == order(i)) then
-          musician = room%musicians(i)
-          room%musicians(i) = room%musicians(j)
-          room%musicians(j) = musician
-          exit
-        end if
+      room%musicians(i)%pos%x = -2000
+      order(i) = room%musicians(i)%instrument
+    end do
+    do instr = 1, room%N_instruments
+      musician%instrument = instr
+      do i = 1, size(grid(instr)%skip)
+        musician%pos = grid(instr)%pos(i)
+        room%musicians(1) = musician
+        grid(instr)%value = room%score()
       end do
     end do
-  end subroutine restore_order
+    do t = 1, room%N_musicians
+      layer = order(t)
+      max_pos = maxloc(grid(layer)%value)
+      room%musicians(t)%instrument = order(t)
+      room%musicians(t)%pos = grid(layer)%pos(max_pos(1))
+      do instr = 1, room%N_instruments
+        call grid(instr)%exclude(grid(layer)%pos(max_pos(1)), -1e9_8)
+      end do
+    end do
+  end subroutine guess_v2
 end module guess_mod
