76d83dc247b2951fd7621d9f7f961acd594d2b13
Friedrich von Never <friedrich@fornever.me>
2023-07-10 00:11:22 +0200

    Script to apply suosi

--- scripts/improve-all.ps1
+++ scripts/improve-all.ps1
@@ -3,8 +3,10 @@
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
+dotnet build --configuration Release
+
 $From..$To | ForEach-Object -Parallel {
     Write-Host "Improving $_"
-    dotnet run --configuration Release --project Icfpc2023 -- improve $_ lambda --preserve-best
+    dotnet run --configuration Release --project Icfpc2023 -- improve $_ $Improver --preserve-best
     Write-Host "Improving $_"
 }
