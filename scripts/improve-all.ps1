3802b6b378b156b01be025d91c7df78cc10d477a
Friedrich von Never <friedrich@fornever.me>
2023-07-10 00:13:38 +0200

    Improve the improver script (broken closure wtf)

--- scripts/improve-all.ps1
+++ scripts/improve-all.ps1
@@ -1,4 +1,4 @@
-﻿param ($Improver = 'lambda', $From = 1, $To = 90)
+﻿param ($From = 1, $To = 90)
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
@@ -7,6 +7,6 @@ dotnet build --configuration Release
 
 $From..$To | ForEach-Object -Parallel {
     Write-Host "Improving $_"
-    dotnet run --no-build --configuration Release --project Icfpc2023 -- improve $_ $Improver --preserve-best
+    dotnet run --no-build --configuration Release --project Icfpc2023 -- improve $_ 'suosi' --preserve-best
     Write-Host "Improving $_"
 }
