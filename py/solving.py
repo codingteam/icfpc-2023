22a534aa5fbf28e9ab9e21a3aa2dd27738432321
Akon32 <akon32@rambler.ru>
2023-07-10 00:03:23 +0300

    ES configuration.

--- py/solving.py
+++ py/solving.py
@@ -23,22 +23,22 @@ def ES_solve(stage: Stage,
              pillar_center_radius: np.ndarray,
              use_playing_together_ext: bool = True) -> (np.ndarray, float, bool):
     initial_sigmas = np.array([stage.w / 3 , stage.h / 3 , 5 / 3 ], dtype='float64')
-    step_count = 100
-    population_size = 20
+    step_count = 1000
+    population_size = 4
     score_k = 1
     mus_dist_penalty_k = -1e9
     mus_scene_penalty_k = -1e9
-    mutation_probability = 0.2
+    mutation_probability = 0.5
 
     n_musicians = mus_instruments.shape[0]
 
     def call_score(mus_places_volumes: np.ndarray) -> (float, bool, float, float):
         sc = mc_score(mus_instruments, mus_places_volumes, att_places, att_tastes, pillar_center_radius,
                       use_playing_together_ext,
-                      mus_ratio=0.01,
-                      att_ratio=0.01,
-                      pillar_ratio=0.01,
-                      n_eval=30)
+                      mus_ratio=1,
+                      att_ratio=1,
+                      pillar_ratio=1,
+                      n_eval=1)
         mus_dist_penalty = musicians_distance_penalty(mus_places_volumes)
         mus_scene_penalty = musicians_out_of_scene_penalty(stage, mus_places_volumes)
         valid = mus_dist_penalty <= 0 and mus_scene_penalty <= 0
@@ -59,15 +59,16 @@ def ES_solve(stage: Stage,
     current_solution = initial_solution(stage, n_musicians, [0.001, 0.001, 0.1])
     current_score = call_score(current_solution)
     for step in range(step_count):
-        # sigmas = initial_sigmas * (1.0 - step / step_count)
-        sigmas = initial_sigmas * (math.exp(-step / step_count * 6))
+        sigmas = initial_sigmas * (1.0 - step / step_count)
+        # sigmas = initial_sigmas * (math.exp(-step / step_count * 4))
+        mutation_probability = 0.25*(1.0 - step / step_count)
         print(f"Step {step}/{step_count} Approx.score={current_score} sigmas={sigmas}")
         population = gen_population(current_solution, sigmas)
         # TODO: make solutions valid / filter invalid
 
         for solution in population:
             sol_score = call_score(solution)
-            current_score = call_score(current_solution)
+            # current_score = call_score(current_solution)
             if sol_score[0] > current_score[0] and (step < step_count * 0.7 or sol_score[1] >= current_score[1]):
                 current_solution = solution
                 current_score = sol_score
