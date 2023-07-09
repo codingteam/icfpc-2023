f07ecae5adfc2dac60af5b27189db7470342c266
Friedrich von Never <friedrich@fornever.me>
2023-07-09 16:34:37 +0200

    Script for mass improvement (doesn't work well) and improved #42

---
+++ scripts/improve-all.ps1
@@ -0,0 +1,10 @@
+﻿param ($Improver = 'lambda', $From = 1, $To = 90)
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+$From..$To | ForEach-Object -Parallel {
+    Write-Host "Improving $_"
+    dotnet run --configuration Release --project Icfpc2023 -- improve $_ lambda --preserve-best
+    Write-Host "Improving $_"
+}
